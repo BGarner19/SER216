d0c5faccf89323368561f866e252097b621077af
BAILEY-PC\Bailey <bailey.garner@asu.edu>
2018-01-24 01:29:35 -0700

    Initial GUI setup

--- Assignment 1/src/Assignment1.java
+++ Assignment 1/src/Assignment1.java
@@ -1,7 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Assignment1 extends JFrame {
+public class Assignment1 extends JFrame implements ActionListener {
+
+    private JTextField binaryText;
+    private JTextField binaryToDecimalText;
+    private JTextField hexText;
+    private JTextField hexToDecimalText;
+    private JButton convertBinary;
+    private JButton convertHex;
+
+    public Assignment1() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Assignment1 frame = new Assignment1();
+        frame.pack();
+        frame.setTitle("Assignment 1: Exception Handling");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+
     }
 }
