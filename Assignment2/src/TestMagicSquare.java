92d322dd094c2ea11a408061f192148bbc95de55
Bailey Garner <bailey.garner@asu.edu>
2018-01-29 14:17:44 -0700

    Setup logic to read numbers and check if input is valid square

--- Assignment2/src/TestMagicSquare.java
+++ Assignment2/src/TestMagicSquare.java
@@ -8,7 +8,34 @@ public class TestMagicSquare {
         ArrayList<Integer> squareNumbers = new ArrayList<Integer>();
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Please enter a list of numbers: ");
+        System.out.print("Please enter a list of numbers terminated by any non-integer character: ");
 
+        while (scan.hasNextInt()) {
+            squareNumbers.add(scan.nextInt());
+        }
+
+        if (isSquare(squareNumbers)) {
+
+            MagicSquare newSquare = new MagicSquare(squareNumbers);
+
+            if (newSquare.isMagic()) {
+                System.out.println("true");
+            }
+            else {
+                System.out.println("false");
+            }
+        }
+        else {
+            System.out.println("false");
+        }
+
+
+    }
+
+    public static boolean isSquare(ArrayList<Integer> numbers) {
+
+        int root = (int) Math.sqrt(numbers.size());
+
+        return (root * root == numbers.size());
     }
 }
